948971d10fcb4738c9dfefa6f35230bec9ee05cf
Lucas Ledsham <lucasnunesleal@gmail.com>
2025-08-29 16:07:10 -0300

    Botao Contato todas paginas

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -1,12 +1,12 @@
-function abrirFormulario() {
-  document.getElementById("fundoContato").style.display = "flex";
-}
+// function abrirFormulario() {
+//   document.getElementById("fundoContato").style.display = "flex";
+// }
 
-function fecharFormulario() {
-  document.getElementById("fundoContato").style.display = "none";
-}
+// function fecharFormulario() {
+//   document.getElementById("fundoContato").style.display = "none";
+// }
 
-// Fecha ao clicar fora da caixa
-document.getElementById("fundoContato").addEventListener("click", function () {
-  fecharFormulario();
-});
+// // Fecha ao clicar fora da caixa
+// document.getElementById("fundoContato").addEventListener("click", function () {
+//   fecharFormulario();
+// });
